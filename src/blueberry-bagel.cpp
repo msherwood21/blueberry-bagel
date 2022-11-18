27e93506c81edc5dd38c40b5db4390ed3d3501c4
Matt Sherwood <msherwood21@gmail.com>
2022-11-18 16:20:33 -0600

    Added code to alert if the sensor has reported an error.

--- src/blueberry-bagel.cpp
+++ src/blueberry-bagel.cpp
@@ -2,9 +2,12 @@
 #include "Adafruit_CCS811.h"
 
 Adafruit_CCS811 ccs;
+size_t const ErrorLED = 2;
 
 void setup() {
     Serial.begin(9600);
+
+    pinMode(ErrorLED, OUTPUT);
 
     if(!ccs.begin()) {
         Serial.println("Failed to start sensor! Please check your wiring.");
@@ -13,10 +16,23 @@ void setup() {
 }
 
 void loop() {
-    if(ccs.available() && !ccs.readData()) {
-        Serial.print(ccs.geteCO2());
-        Serial.print(" ");
-        Serial.println(ccs.getTVOC());
+    static bool checkSensor = true;
+    static size_t errorPosition = LOW;
+
+    if(!checkSensor) {
+        digitalWrite(ErrorLED, errorPosition);
+        errorPosition = errorPosition ? LOW : HIGH;
+    } else {
+        if(ccs.available()) {
+            if(!ccs.readData()) {
+                Serial.print(ccs.geteCO2());
+                Serial.print(" ");
+                Serial.println(ccs.getTVOC());
+            } else {
+                Serial.println("Error reading data. Stopping.");
+                checkSensor = false;
+            }
+        }
     }
 }
 
